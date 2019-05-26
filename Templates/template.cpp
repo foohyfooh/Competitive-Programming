cfeeb83cb2d0997fca9b6199ee350ff213563863
Jonathan Herbert <foohyfooh@gmail.com>
2019-05-25 22:12:18 -0400

    Add Templates

---
+++ Templates/template.cpp
@@ -0,0 +1,16 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lld;
+typedef unsigned long long int llu;
+#define make_pair mp
+
+int main(int argc, char** argv){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return EXIT_SUCCESS;
+}
